08e87c694404e5f622f8699bd7d7e6e1940580f9
Prathamesh Tatar <>
2024-12-20 18:21:04 +0530

    Fix init issue

--- Sources/spm-greetings/SPMGreetings.swift
+++ Sources/spm-greetings/SPMGreetings.swift
@@ -1,4 +1,7 @@
 public class SPMGreetings {
+    // Add public init to enable creation of instance of this class
+    public init() {}
+
     public func greetWorld() -> String {
         return "Hello, world!"
     }
