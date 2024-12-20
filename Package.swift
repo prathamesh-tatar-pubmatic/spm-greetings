08ce9f084b7bd6fb0792ea1a73853e390e042a91
Prathamesh Tatar <>
2024-12-20 17:47:21 +0530

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,17 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "spm-greetings",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(name: "spm-greetings", targets: ["spm-greetings"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(name: "spm-greetings")
+    ]
+)
